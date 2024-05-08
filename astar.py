f28d40bba6fcd3a954c1492533d6d901c05af966
Aditi Vasmatkar <100767751+Aditiv12@users.noreply.github.com>
2024-05-08 23:59:32 +0530

    Update astar.py

--- astar.py
+++ astar.py
@@ -1,59 +1,58 @@
-import collections
-#A*
-from collections import deque
+import queue
 
-class graph:
-    def __init__(self, adjacency_list):
-        self.adjacency_list = adjacency_list
-    def get_neighbours(self, v):
-        return self.adjacency_list[v]
-    def h(self, n):
-        H ={ 'A':1, 'B':1, 'C':1, 'D':1 }
-        return H[n]
-    def a_star (self, start_node, stop_node):
-        open_list =set([start_node])
-        closed_list =set([])
-        g ={}
-        g[start_node]=0
-        parents ={}
-        parents[start_node] =start_node
+alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+start='A'
+goal='D'
+result=''
+closed=[]
+path=[]
+n = int(input("Enter number of vertices: "))
+dict_gn={ alpha[i]:{} for i in range(n) }
+dict_hn={ alpha[i]:0 for i in range(n) }
+print("Enter directed edges with weights below: (press enter to skip edges that are not present)")
+for i in range(n):
+    for j in range(n):
+        if(i!=j):
+            try:
+                parseIn=int(input(f"Enter weight for edge {alpha[i]}-{alpha[j]}: "))
+            except ValueError:
+                continue
+            dict_gn[alpha[i]][alpha[j]]=parseIn
+for i in range(n):
+    try:
+        dict_hn[alpha[i]] = int(input(f"Enter hueristic value of node {alpha[i]}: "))
+    except ValueError:
+        print("Invalid value:")
 
-        while len(open_list)>0:
-            n=None
-            for v in open_list:
-                if n==None or g[v] + self.h(v) < g[n] +self.h(n):
-                    n=v
-            if n==None:
-                print('Path does not exist')
-                return None
-            if n==stop_node:
-                reconst_path =[]
-                while parents[n] !=n:
-                    reconst_path.append(n)
-                    n=parents[n]
-                reconst_path.append(start_node)
-                reconst_path.reverse()
-                print('Path found: {}'.format(reconst_path))
-                return reconst_path
-            for (m, weight) in self.get_neighbours(n):
-                if m not in open_list and m not in closed_list:
-                    open_list.add(m)
-                    parents[m]=n
-                    g[m]=g[n]+weight
-                else:
-                    if g[m]>g[n]+weight:
-                        g[m]=g[n]+weight
-                        parents[m]=n
-                if m in closed_list:
-                    closed_list.remove(m)
-                    open_list.add(m)
-            open_list.remove(n)
-            closed_list.add(n)
-        print('Path does not exist')
-        return None
-if __name__ == '__main__':    
-    adjacency_list={
-        'A':[('B',1), ('C',3), ('D',7)], 'B':[('D', 5)], 'C':[('D', 12)]
-    }
-    graph1 =graph(adjacency_list)
-    graph1.a_star('A', 'D')
+start = input("Enter the start node alphabet: ").upper()
+goal = input("Enter the goal node alphabet: ").upper()
+
+print(f'G(n): {dict_gn}')
+print(f"H(n): {dict_hn}")
+
+def expand(cityq):
+    global result
+    global closed
+    while not cityq.empty():
+        tot, thiscity, g_thisCity=cityq.get()
+        closed.append(thiscity)
+        path.append(thiscity)
+        if thiscity==goal:
+            result="-> ".join(path)
+            result+='\nTotalCost: {}'.format(tot)
+            return 1
+        for cty, gn in dict_gn[thiscity].items():
+            if cty not in closed:
+                cityq.put((dict_hn[cty]+g_thisCity+gn, cty, g_thisCity+gn)) #(Fn, current_city, Gn_tillCurrCity)
+        print(f'Queue After processing {thiscity}: ', cityq.queue)
+    return 0
+def main():
+    cityq=queue.PriorityQueue()
+    thiscity=start
+    cityq.put((0,thiscity, 0)) # Setting highest priority to the start city so it get's popped. 
+    if(expand(cityq)):
+        print("The A* path with the total cost is: ")
+        print(result)
+    else:
+        print("A* couldn't find a path :/")
+main()
